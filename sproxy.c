1fe6d8056195542285045f39706112b41dba009c
mtl11 <62129086+mtl11@users.noreply.github.com>
2022-02-27 14:40:00 -0700

    sproxy correct

--- sproxy.c
+++ sproxy.c
@@ -4,68 +4,65 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-int main(int argc, char* argv[]){
-    //structs to recieve from telnet
+int main(int argc, char* argv[]) {
+////////////////////////////////////////////////////////
+//      Send/Recieve (Cproxy <-> Sproxy) Socket       //
+////////////////////////////////////////////////////////
+
     int sockfd, newsockfd, portno, clilen;
-    struct sockaddr_in serv_addr, cli_addr, serv_addrProxy;
-    // structs to send from here to client
-    int cliSockfd, cliPortno;
-    struct hostent *server;
-    char buffer[1024];
+    struct sockaddr_in serv_addr, cli_addr;
     
-    if (argc < 4){
+    if (argc < 2)
+    {
         fprintf(stderr, "ERROR, no port provided\n");
         exit(1);
     }
-////////////////////////////////////////////////////////
-//           Send/Recieve (Telnet <-> Cproxy) Socket        //
-////////////////////////////////////////////////////////
-
+    
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    if (sockfd < 0){fprintf(stderr, "ERROR, bad socket\n");exit(1);}
-    
+    if (sockfd < 0)
+    {
+        fprintf(stderr, "ERROR, bad socket\n");
+        exit(1);
+    }
     bzero((char *) &serv_addr,sizeof(serv_addr));
 
     portno = atoi(argv[1]);
+
     serv_addr.sin_family=AF_INET;
     serv_addr.sin_addr.s_addr=INADDR_ANY;
     serv_addr.sin_port=htons(portno);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr))<0){error("binding");}
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr))<0)
+    {
+        error("binding");
+    }
     listen(sockfd, 5);
     clilen = sizeof(cli_addr);
 
-    size_t len;
-    int buf_len;
-
     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-  ////////////////////////////////////////////////////////
- //              Send/Recieve (Cproxy <-> Sproxy) Socket        //
-  ////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+//   Send/Recieve (Sproxy <-> Telnet Daemon) Socket    //
+////////////////////////////////////////////////////////
 
-    cliPortno = atoi(argv[3]);
+    struct sockaddr_in daemon_address;
+    int d_sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    cliSockfd = socket(AF_INET, SOCK_STREAM, 0);
+    daemon_address.sin_family=AF_INET;
+    daemon_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    daemon_address.sin_port=htons(23);
 
-    if (cliSockfd < 0){fprintf(stderr, "ERROR, bad socket\n");exit(1);}
-
-    server = gethostbyname(argv[2]);
-    if (server == NULL){fprintf(stderr, "ERROR, bad server\n");exit(1);}
-
-    bzero((char *) &serv_addrProxy, sizeof(&serv_addrProxy));
-    serv_addrProxy.sin_family = AF_INET;
-    bcopy((char *) server->h_addr, (char *)&serv_addrProxy.sin_addr.s_addr, server->h_length);
-    serv_addrProxy.sin_port = htons(cliPortno);
-
-    if (connect(cliSockfd, &serv_addrProxy, sizeof(serv_addrProxy))<0){fprintf(stderr, "ERROR, bad connection\n");exit(1);}
-    fd_set readfds;
+    if (connect(d_sock, (struct sockaddr *) &daemon_address, sizeof(serv_addr))<0){
+        perror("Connection Failed");
+        exit(0);
+    }
 
 ////////////////////////////////////////////////////////
 //              Select where data will go             //
 ////////////////////////////////////////////////////////
-    int sock1_telnet = newsockfd;
-    int sock2_sproxy = cliSockfd;
+    fd_set readfds;
+    int sock1_telnet = d_sock;
+    int sock2_sproxy = newsockfd;
 
     FD_ZERO(&readfds);
     FD_SET(sock1_telnet, &readfds);
@@ -83,23 +80,23 @@ int main(int argc, char* argv[]){
     tv.tv_usec = 500000;
     rv = select(n, &readfds, NULL, NULL, &tv);
 
-
     if (rv == -1) {
         perror("select");
     } else if (rv == 0) {
         printf("Timeout occurred!  No data after 10.5 seconds.\n");
     } else {
         if (FD_ISSET(sock1_telnet, &readfds)) {
-            fputs("Sent to proxy", stdout);
+            fputs("Sent to proxy\n", stdout);
             len1 = recv(sock1_telnet, cmd_buffer, 1024, 0);
             send(sock2_sproxy, cmd_buffer, 1024, 0);
         }
         if (FD_ISSET(sock2_sproxy, &readfds)) {
-            fputs("Sent to telnet", stdout);
+            fputs("Sent to telnet\n", stdout);
             len2 = recv(sock2_sproxy, reply_buffer, 1024, 0);
             send(sock1_telnet, reply_buffer, 1024, 0);
         }
     }
-    close(newsockfd);
+    close(sockfd);
+    close(d_sock);
 return 0;
 }
